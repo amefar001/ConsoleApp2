ccaa1289218eb823bc9fe3b957ded0dff7f02a8e
ameen <ameen@AMEEN>
2024-09-11 12:01:38 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exempel_1._3___Specialtecken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Här skrivs ett \"-tecken ut.");
+            Console.WriteLine("Ett bakstreck \\ syns här.");
+            Console.WriteLine("Här blir det \n en ny rad och \t en tab.");
+
+        }
+    }
+}
